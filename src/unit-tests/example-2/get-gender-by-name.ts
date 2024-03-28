bf37405c189cbaa155a0d1ee0b3d7e02776d4609
Jika1995 <jikulina2016@gmail.com>
2024-03-28 16:35:38 +0600

    add unit tests

--- src/unit-tests/example-2/get-gender-by-name.ts
+++ src/unit-tests/example-2/get-gender-by-name.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'https://api.genderize.io';
+const NAME_REGEX = /^[A-Za-z]+$/;
 
 type GenderResponse = {
   name: string;
@@ -10,7 +11,14 @@ type GenderResponse = {
 }
 
 const getGenderByName = async (name: string): Promise<GenderResponse | Error> => {
+  if (!NAME_REGEX.test(name)) {
+    throw new Error('Invalid name, only EN letters are allowed');
+  };
   const { data: gender } = await axios.get<GenderResponse>(`${ API_URL }?name=${ name }`);
+
+  if (gender.probability < 0.9) {
+    throw new Error(`Probability is less than 90%, value received = ${ gender.probability }`);
+  }
   return gender;
 };
 
