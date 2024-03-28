56a5113dd4af276b8476ea30f8ab5f5fa0b9215d
Jika1995 <jikulina2016@gmail.com>
2024-03-28 17:15:31 +0600

    add integration, e2e tests and coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,20 @@
 module.exports = {
   testEnvironment: 'node',
   preset: 'ts-jest',
-  roots: ['<rootDir>/src'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  roots: ['./src'],
+  silent: false,
+  verbose: true,
+  collectCoverageFrom: ['src/**'],
+  coverageReporters: ['text'],
+  coverageThreshold: {
+    global: {
+      lines: 98
+    }
+  }
+  // roots: ['<rootDir>/src'],
+  // transform: {
+  //   '^.+\\.tsx?$': 'ts-jest',
+  // },
+  // testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  // moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 }
